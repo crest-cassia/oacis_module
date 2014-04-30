31c9e7a4515ce46facd714b4de01b3fa485f931d
Takeshi Uchitane <takeshi.uchitane@riken.jp>
2014-04-30 15:25:31 +0900

    enable to load test environment from oacis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
-require "rubygems"
-require "bundler/setup"
-require 'rspec'
-Dir[File.join(File.dirname(__FILE__), "..", "app", "**/*.rb")].each{|f| require f }
+ENV['BUNDLE_GEMFILE'] = File.join(File.dirname(__FILE__), '../Gemfile')
+require 'bundler/setup'
+require "#{ENV["OACIS_HOME"]}/config/environment"
+require 'database_cleaner'
+require 'factory_girl_rails'
+paths = FactoryGirl.definition_file_paths
+FactoryGirl.definition_file_paths = paths.map {|path| "#{ENV["OACIS_HOME"]}/#{path}"}
+FactoryGirl.find_definitions
+require "#{ENV["OACIS_HOME"]}/spec/spec_helper"
+
