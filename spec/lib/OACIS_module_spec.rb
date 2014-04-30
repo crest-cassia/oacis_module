f2b221536fdc13deb0d7626de2fd7119e5db4366
Takeshi Uchitane <takeshi.uchitane@riken.jp>
2014-04-30 16:22:11 +0900

    update test for OACIS_module#initialize

--- spec/lib/OACIS_module_spec.rb
+++ spec/lib/OACIS_module_spec.rb
@@ -1,10 +1,48 @@
 require 'spec_helper'
 
-describe "sample" do
+describe "OACIS_module" do
 
-  it "return false" do
+  describe "#initialize" do
 
-    b = false
-    b.should eq false
+    before(:each) do
+      @sim = FactoryGirl.create(:simulator, parameter_sets_count: 2, runs_count: 0, finished_runs_count: 2, analyzers_count: 1, run_analysis: false, analyzers_on_parameter_set_count: 1, run_analysis_on_parameter_set: false)
+      @valid_attr = {"_target"=>{"Simulator"=>@sim.to_param}}
+    end
+
+    it "needs an argument with Hash[\"_target""][\"Simulator\"]=sim.id" do
+
+      expect {
+        oacis_module = OacisModule.new(@valid_attr)
+      }.not_to raise_error
+      class OacisModule
+        def target_sim
+          @target_simulator
+        end
+      end
+      OacisModule.new(@valid_attr).target_sim.should eq @sim
+    end
+
+    context "when [\"_target""][\"Simulator\"] is invalid" do
+
+      it "raise error" do
+        @valid_attr["_target"]["Simulator"]="0123456789"
+        expect {
+          OacisModule.new(@valid_attr)
+        }.to raise_error
+      end
+    end
+
+    it "perses json values to hash" do
+
+      class OacisModule
+        def input_data
+          @input_data
+        end
+      end
+      data = [{"type"=>"String"},{"value"=>12345}]
+      @valid_attr["json"]=data.to_json
+      oacis_module = OacisModule.new(@valid_attr)
+      oacis_module.input_data["json"].should eq data
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,5 @@ paths = FactoryGirl.definition_file_paths
 FactoryGirl.definition_file_paths = paths.map {|path| "#{ENV["OACIS_HOME"]}/#{path}"}
 FactoryGirl.find_definitions
 require "#{ENV["OACIS_HOME"]}/spec/spec_helper"
+Dir[File.join(File.dirname(__FILE__), "..", "lib", "**/*.rb")].each{|f| require f }
 
