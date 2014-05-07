11f58cbc701d7a809b08e563a727115070882c6c
Takeshi Uchitane <takeshi.uchitane@riken.jp>
2014-05-07 15:30:25 +0900

    a serious bug is fixed: do not destroy collections in oacis_development

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,7 @@
 ENV['BUNDLE_GEMFILE'] = File.join(File.dirname(__FILE__), '../Gemfile')
 require 'bundler/setup'
+ENV["RAILS_ENV"] = 'test'
 require "#{ENV["OACIS_HOME"]}/config/environment"
-require 'database_cleaner'
-require 'factory_girl_rails'
-paths = FactoryGirl.definition_file_paths
-FactoryGirl.definition_file_paths = paths.map {|path| "#{ENV["OACIS_HOME"]}/#{path}"}
-FactoryGirl.find_definitions
 require "#{ENV["OACIS_HOME"]}/spec/spec_helper"
 Dir[File.join(File.dirname(__FILE__), "..", "lib", "**/*.rb")].each{|f| require f }
 
